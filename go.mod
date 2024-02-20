e044dee4158d446dd180679744f87ecfd8629a9b
Albert Fazlyev <albert.fz@yandex.ru>
2024-02-20 18:06:39 +0500

    implement basic log parsing from nginx via syslogd

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/bulatovv/mera
 
 go 1.22.0
+
+require github.com/influxdata/go-syslog/v3 v3.0.0 // indirect
--- mera.go
+++ mera.go
@@ -1,3 +1,59 @@
 package main
 
-func main() {}
+import (
+	"encoding/json"
+	"log"
+	"net"
+	"github.com/influxdata/go-syslog/v3/rfc3164"
+)
+
+type logEntry struct {
+    IP string `json:"ip"`
+    URL string `json:"url"`
+    Host string `json:"host"`
+    Referrer string `json:"referrer"`
+    UserAgent string `json:"user_agent"`
+    Time string `json:"time"`
+}
+
+func main() {
+    pc, err := net.ListenPacket("udp", ":1234")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer pc.Close()
+    
+    p := rfc3164.NewParser()
+
+
+    for {
+        buf := make([]byte, 2048)
+        n, _, err := pc.ReadFrom(buf)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        msg, err := p.Parse(buf[:n])
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        rfc3164 := msg.(*rfc3164.SyslogMessage)
+
+        log.Println(*rfc3164.Message)
+
+        var entry logEntry
+        err = json.Unmarshal(
+            []byte(*rfc3164.Message),
+            &entry,
+        )
+        if err != nil {
+            log.Fatal(err)
+        }
+
+
+    }
+
+    
+
+}
